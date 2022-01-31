49d8289aca9189010e8d76e53c98f90e21ede0cb
neerajsinghal933 <neerajsinghal933@gmail.com>
2022-02-01 00:12:47 +0530

    doctorsFromUpdated

--- BTP/doctors/models.py
+++ BTP/doctors/models.py
@@ -1,36 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import User  # user used by django for authetication and login in admin page
+# user used by django for authetication and login in admin page
+from django.contrib.auth.models import User
 # check django user model for more information
 # Create your models here.
 import uuid
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from phone_field import PhoneField
+import datetime
 
 # from phonenumber_field.modelfields import PhoneNumberField
 
 
-
 class Profile(models.Model):
     GenderChoices = (
-    ("M", "Male"),
-    ("F", "Female"),
+        ("M", "Male"),
+        ("F", "Female"),
     )
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null = True, blank = True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     # user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
     # ANytime user is deleted the profile is deleted
-    name = models.CharField(max_length=200, blank = True, null = True)
+    name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
-    gender = models.CharField(max_length=1, choices = GenderChoices, blank = False, null = False)
-    age = models.IntegerField(null = False)
-    available_time = models.OneToOneField('Availability', on_delete=models.CASCADE, null = True, blank = True)
-    qualifications = models.ManyToManyField('Qualification', blank = True)
-    username = models.CharField(max_length=200, blank = True, null = True)
-    location = models.CharField(max_length=500, blank = True, null = True)
-    short_intro = models.CharField(max_length=200, blank = True, null = True)
-    bio = models.TextField(blank = True, null = True)
-    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default="profiles/user-default.png")
-    speciality = models.CharField(max_length=200, blank = True, null = True)
+    gender = models.CharField(
+        max_length=1, choices=GenderChoices, blank=False, null=False)
+    age = models.IntegerField(null=False)
+    available_time = models.OneToOneField(
+        'Availability', on_delete=models.CASCADE, null=True, blank=True)
+    qualifications = models.ManyToManyField('Qualification', blank=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=500, blank=True, null=True)
+    short_intro = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(
+        null=True, blank=True, upload_to='profiles/', default="profiles/user-default.png")
+    speciality = models.CharField(max_length=200, blank=True, null=True)
     contact_number = PhoneField(blank=True, help_text='Contact phone number')
 
     # We want specifically a folder for profile pictures. SO in static in mages we create a new folder here
@@ -40,39 +45,40 @@ class Profile(models.Model):
     # social_linkedin = models.CharField(max_length=200, blank = True, null = True)
     # social_youtube = models.CharField(max_length=200, blank = True, null = True)
     # social_website = models.CharField(max_length=200, blank = True, null = True)
-    joining_date = models.DateField(auto_now_add = True)
-    Did = models.UUIDField(default=uuid.uuid4, unique=True, primary_key = True, editable = False)
-    
+    joining_date = models.DateField(auto_now_add=True)
+    Did = models.UUIDField(default=uuid.uuid4, unique=True,
+                           primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.username)
 
+
 class Qualification(models.Model):
-    degree_name = models.CharField(max_length =200)
+    degree_name = models.CharField(max_length=200)
     degree_date = models.DateField()
     university = models.CharField(max_length=200)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key = True, editable = False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
     # id = models.UUIDField(default=generateUUID, unique=True, primary_key = True, editable = False)
 
     def __str__(self):
-        return self.name
+        return self.degree_name
 
 
 class Availability(models.Model):
-    monday_from = models.TimeField(null = True, blank = True)
-    monday_to = models.TimeField(null = True, blank = True)
-    tuesday_from = models.TimeField(null = True, blank = True)
-    tuesday_to = models.TimeField(null = True, blank = True)
-    wednesday_from = models.TimeField(null = True, blank = True)
-    wednesday_to = models.TimeField(null = True, blank = True)
-    thursday_from = models.TimeField(null = True, blank = True)
-    thursday_to = models.TimeField(null = True, blank = True)
-    friday_from = models.TimeField(null = True, blank = True)
-    friday_to = models.TimeField(null = True, blank = True)
-    saturday_from = models.TimeField(null = True, blank = True)
-    saturday_to = models.TimeField(null = True, blank = True)
-    sunday_from = models.TimeField(null = True, blank = True)
-    sunday_to = models.TimeField(null = True, blank = True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key = True, editable = False)
-    
-    
+    monday_from = models.TimeField(default=datetime.time(16, 00))
+    monday_to = models.TimeField(default=datetime.time(16, 00))
+    tuesday_from = models.TimeField(default=datetime.time(16, 00))
+    tuesday_to = models.TimeField(default=datetime.time(16, 00))
+    wednesday_from = models.TimeField(default=datetime.time(16, 00))
+    wednesday_to = models.TimeField(default=datetime.time(16, 00))
+    thursday_from = models.TimeField(default=datetime.time(16, 00))
+    thursday_to = models.TimeField(default=datetime.time(16, 00))
+    friday_from = models.TimeField(default=datetime.time(16, 00))
+    friday_to = models.TimeField(default=datetime.time(16, 00))
+    saturday_from = models.TimeField(default=datetime.time(16, 00))
+    saturday_to = models.TimeField(default=datetime.time(16, 00))
+    sunday_from = models.TimeField(default=datetime.time(16, 00))
+    sunday_to = models.TimeField(default=datetime.time(16, 00))
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
--- BTP/doctors/views.py
+++ BTP/doctors/views.py
@@ -8,13 +8,10 @@ from django.utils.datastructures import MultiValueDictKeyError
 
 def doctorRegister(request, pk):
     msg = None
-    usr = Profile.objects.get(Did = pk)
-    print(pk)
-    print('HI')
-    print(usr.Did)
-    form = DoctorProfileForm(instance = usr)
+    usr = Profile.objects.get(Did=pk)
+    form = DoctorProfileForm(instance=usr)
     if request.method == 'POST':
-        form = DoctorProfileForm(request.POST, request.FILES, instance = usr)
+        form = DoctorProfileForm(request.POST, request.FILES, instance=usr)
         if(form.is_valid()):
             # pk = request.POST.get('Did')
             form.save()
@@ -28,46 +25,54 @@ def doctorRegister(request, pk):
     #     msg = 'Not found'
     return render(request, 'doctorRegister.html', {'form': form, 'msg': msg})
 
+
 def doctorRegister2(request, pk):
-    user = Profile.objects.get(Did = pk)
+    user = Profile.objects.get(Did=pk)
     # form = DoctorAvailablityForm(instance = user)
 
     form = DoctorAvailablityForm()
     if request.method == 'POST':
         # form = DoctorAvailablityForm(request.POST, instance = user)
         form = DoctorAvailablityForm(request.POST)
-        try:
-            if(form.is_valid()):
-                info = request.POST
-                user.available_time = info
-                user = user.save(commit = False) 
-                user = user.Did
-        except MultiValueDictKeyError:
-            info = False
+        # try:
+        if(form.is_valid()):
+            avail = form.save()
+            user.available_time = avail
+            user.save()
+            # info = request.POST
+            # user.available_time = info
+            # user = user.save(commit = False)
+            # user = user.Did
+        # except MultiValueDictKeyError:
+        #     info = False
         form.save()
-        return redirect('doctorRegister3', user)
-    context = {'form':form}
+        return redirect('doctorRegister3', pk)
+    context = {'form': form}
     return render(request, 'doctorRegister2.html', context)
 
+
 def doctorRegister3(request, pk):
-    user = Profile.objects.get(Did = pk)
+    user = Profile.objects.get(Did=pk)
     # form = DoctorQualificationForm(instance = user)
     form = DoctorQualificationForm()
     if request.method == 'POST':
         form = DoctorQualificationForm(request.POST)
         if(form.is_valid()):
-            info = request.POST
+
+            info = form.save()
             user.qualifications = info
             user.save()
             # user.available_time
             form.save()
+
             return redirect('doctorLogin')
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'doctorRegister3.html', context)
 
 
 def doctorProfile(request):
     return render(request, 'doctorProfile.html')
+
 
 def doctorLogin(request):
     return render(request, 'doctorLogin.html')
