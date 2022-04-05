38db03d2ff891a82a8daceb7e66261921df821b7
neerajsinghal933 <neerajsinghal933@gmail.com>
2022-04-06 02:19:47 +0530

    main page updated

--- BTP/users/models.py
+++ BTP/users/models.py
@@ -39,3 +39,27 @@ class TempClientProfile(models.Model):
     def __str__(self):
         return str(self.username)
 
+class TempDoctorProfile(models.Model):
+    GenderChoices = (
+        ("M", "Male"),
+        ("F", "Female"),
+    )
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null = True, blank = True)
+    # user = models.OneToOneField(
+    #     User, on_delete=models.CASCADE, null=True, blank=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
+    # ANytime user is deleted the profile is deleted
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    gender = models.CharField(
+        max_length=1, choices=GenderChoices, blank=False, null=False)
+    dob = models.DateField()
+    username = models.CharField(max_length=200, blank=True, null=True)
+    TempDid = models.UUIDField(default=uuid.uuid4, unique=True,
+                           primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.username)
+
