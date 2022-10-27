14698f7dd1b176a7fe7903dda4cd02a575469498
neerajsinghal933 <neerajsinghal933@gmail.com>
2022-10-27 08:18:38 +0530

    Instructions page added

--- BTP/blogs/forms.py
+++ BTP/blogs/forms.py
@@ -1,6 +1,6 @@
 from django.db.models.fields import Field
 from django.forms import ModelForm
-from .models import Blog
+from .models import Blog, Review
 from django import forms
 
 
@@ -28,3 +28,20 @@ class BlogForm(ModelForm):
         # self.fields["description"].widget.attrs.update(
         #     {"class": "input", "placeholder": "Add Description"}
         # )
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+
+        labels = {
+            'value': 'Place your vote',
+            'body': 'Add a comment with your vote',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+
+        for name, fields in self.fields.items():
+            fields.widget.attrs.update({"class": "input"})
--- BTP/blogs/models.py
+++ BTP/blogs/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 from doctors.models import Profile
 import uuid
@@ -21,6 +22,26 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    class Meta:
+        unique_together = [['owner', 'blog']];
+
+    def __str__(self):
+        return self.value
 
 
 class Tag(models.Model):
